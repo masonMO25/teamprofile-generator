688cfb7acf99fff0aadd88267619cb96a0f14be2
masonMO25 <masonneale@gmail.com>
2022-12-20 18:26:05 -0600

    Added and deleted code in the test files

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,5 @@
-// using Employee constructor 
-const Employee = require('../lib/Employee');
+import { Employee } from "../lib/Employee.js";
 
-// creates employee object 
 test('creates an employee object', () => {
     const employee = new Employee('Ethan', 24, 'weepel@gmail.com');
 
@@ -10,28 +8,24 @@ test('creates an employee object', () => {
     expect(employee.email).toEqual(expect.any(String));
 });
 
-// gets id from getId() 
 test('gets employee name', () => {
     const employee = new Employee('Ethan', 24, 'weepel@gmail.com');
 
     expect(employee.getName()).toEqual(expect.any(String));
 });
 
-// gets id from getId() 
 test('gets employee ID', () => {
     const employee = new Employee('Ethan', 24, 'weepel@gmail.com');
 
     expect(employee.getId()).toEqual(expect.any(Number));
 });
 
-// gets emails from getEmail()
 test('gets employee email', () => {
     const employee = new Employee('Ethan', 24, 'weepel@gmail.com');
 
     expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
 });
 
-// gets role from getRole()
 test('gets role of employee', () => {
     const employee = new Employee('Ethan', 24, 'weepel@gmail.com');
 
