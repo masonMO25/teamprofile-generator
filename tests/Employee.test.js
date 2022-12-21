292b541d056a3f01fdbe7763122ffc8a805c643d
masonMO25 <masonneale@gmail.com>
2022-12-20 23:26:52 -0600

    fixed tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -6,8 +6,4 @@ test('employee', () => {
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-    expect(employee.getName()).toEqual(expect.any(String));
-    expect(employee.getId()).toEqual(expect.any(Number));
-    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
-    expect(employee.getRole()).toEqual("Employee");
 });
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -2,7 +2,6 @@ import Manager from '../lib/Manager.js';
  
 test('Manager', () => {
     const manager = new Manager('Mikey', 404, 'pizza4for4@gmail', 5);
-    
     expect(manager.officeNumber).toEqual(expect.any(Number));
     expect(manager.getRole()).toEqual("Manager");
 });
